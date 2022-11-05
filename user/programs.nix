70583183cc892f975722f318556726759e2d4161
Kostas Andreadis <kandread@umass.edu>
2022-11-05 06:41:00 -0400

    SSH hosts

--- user/programs.nix
+++ user/programs.nix
@@ -26,4 +26,34 @@
       };
     };
 
+    programs.ssh = {
+      enable = true;
+      matchBlocks.delgland = {
+        hostname = "128.119.85.180";
+        user = "kandread";
+      };
+      matchBlocks.thingland = {
+        hostname = "192.168.1.11";
+        user = "kandread";
+      };
+      matchBlocks.adpc = {
+        hostname = "58.137.55.228";
+        user = "ubuntu";
+      };
+      matchBlocks.picore = {
+        hostname = "192.168.1.12";
+        user = "tc";
+      };
+      matchBlocks.amdgland = {
+        hostname = "192.168.1.15";
+        user = "kandread";
+      };
+      matchBlocks.unity = {
+        user = "kandread_umass_edu";
+        hostname = "unity.rc.umass.edu";
+        identityFile = "~/.ssh/unity.key";
+      };
+    };
+
+
 }
