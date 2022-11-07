c63959c00896cf1fcdfb5c756ba39cdad5b3a18c
Kostas Andreadis <kandread@umass.edu>
2022-11-07 04:11:08 -0500

    Add sound clock rates

--- hosts/common.nix
+++ hosts/common.nix
@@ -53,9 +53,15 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+    config.pipewire = {
+      "context.properties" = {
+        "default.clock.allowed-rates" = [ 44100 48000 88200 96000 192000 ];
+      };
+    };
+    wireplumber.enable = false;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
+      # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
 
