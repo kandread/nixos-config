fc77d7a4b95ea9171dc7a4d0c7feed76c7bc5767
Kostas Andreadis <kandread@umass.edu>
2023-01-11 07:01:30 -0500

    Updated key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
   kandread-amd = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTqwBQYrzkMON3F/Q76F78lLHEP2zGCywgJyTbN2Krr";
   kandread-thin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWTSfxDIoCVIN9uVJ0or2zOLL6m7CWsxN3wABR5xjh4";
-  kandread-theli = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICd1aBNsi53jMCy0a9rv+VlS0Ymr6TPM3+MRJj4wMncr"
+  kandread-theli = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJR845DlRwHJcCrHPHZIptWDBhlmTxxNEjF72S1imAHr";
+  theli = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICd1aBNsi53jMCy0a9rv+VlS0Ymr6TPM3+MRJj4wMncr";
   users = [ kandread-amd kandread-thin kandread-theli ];
+  systems = [ theli ];
 in
 {
-  "umass.age".publicKeys = users;
+  "umass.age".publicKeys = users ++ systems;
 }
