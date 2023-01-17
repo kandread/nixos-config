1aba49639a4050c61ac13682e8200a8624081c4a
Kostas Andreadis <kandread@umass.edu>
2023-01-17 15:56:32 -0500

    Set library path

--- hosts/common.nix
+++ hosts/common.nix
@@ -97,6 +97,8 @@
     cudaPackages.cudatoolkit
   ];
 
+  hardware.opengl.setLdLibraryPath = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
