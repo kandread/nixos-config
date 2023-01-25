56abbcb4d94b9b0a5cab57ea1584ab0fd1f4dc70
Kostas Andreadis <kandread@umass.edu>
2023-01-25 13:23:54 -0500

    Changed audio card

--- hosts/common.nix
+++ hosts/common.nix
@@ -121,7 +121,8 @@
     in
       if (config.networking.hostName == "thingland")
       then
-        "-o front:CARD=TA10R,DEV=0 -s ${lms_ip}"
+        # "-o front:CARD=TA10R,DEV=0 -s ${lms_ip}"
+          "-o front:CARD=Audio,DEV=0 -s ${lms_ip}"
       else
         "-o sysdefault:CARD=Generic_1 -s ${lms_ip}";
      };
@@ -140,7 +141,7 @@
     [mattermost]
     DefaultServer = "hydroumass.cloud.mattermost.com"
     DefaultTeam = "main"
-    JoinExclude = ["#.*"]
+    # JoinExclude = ["#.*"]
     PrefixContext = false
     SuffixContext = true
     ThreadContext = "mattermost"
